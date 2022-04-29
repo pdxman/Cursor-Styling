c3a582a071873c5d973187a2f7d5df75019e1025
Crawford Vining <crawpdx@gmail.com>
2022-04-29 16:42:19 -0700

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { useMousePosition } from './useMousePosition';
 
 function App() {
+  const position = useMousePosition()
+  const secondPos = (position.x - position.y)/255
+  const thirdPos = 255/(position.x - position.y)/255
+  const appstyle = {
+    //background: `#${position.x}000`,
+    background: `linear-gradient(90deg, rgba(${position.y},${secondPos},${thirdPos},1) 0%, rgba(${position.x},${position.y},${thirdPos},1) 100%)`,
+    height: '100vh',
+    transition: 'all .5s ease'
+  }
+
+  const pstyle = {
+    fontSize: `${secondPos}em`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={appstyle} className="App">
+      <h1>Cursor Positions</h1>
+      <p style={pstyle}><strong>X:</strong> {position.x}</p>
+      <p style={pstyle}><strong>Y:</strong> {position.y}</p>
+      <p>{secondPos}</p>
+      <p>{thirdPos}</p>
     </div>
   );
 }
